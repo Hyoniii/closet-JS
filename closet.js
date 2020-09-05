d43aa78f8f9c77d3396d889a96bc1f2cb28cdec7
Hyoniii <phhphh841@hanmail.net>
2020-09-05 17:50:32 +0900

    dataset 완성

--- closet.js
+++ closet.js
@@ -1,15 +1,12 @@
 const storeBtn = document.getElementById("JS_store");
-const oneBtn = document.getElementById("JS_shirts");
-const express = require("express");
-
-app.listen(4000);
-
+/*const oneBtn = document.getElementById("JS_shirts");
 const twoBtn = document.getElementById("JS_pants");
 const thrBtn = document.getElementById("JS_skirt");
 const fourBtn = document.getElementById("JS_blue");
 const fiveBtn = document.getElementById("JS_yellow");
-const sixBtn = document.getElementById("JS_pink");
+const sixBtn = document.getElementById("JS_pink");*/
 const listBox = document.getElementById("JS_list");
+const closetList = document.querySelector(".col_two");
 
 const ul = document.getElementById("JS_list");
 
@@ -20,19 +17,44 @@ function loadItems() {
     .then((json) => json.items);
 }
 
-function displayItems(items) {
-  const li = document.createElement("li");
+function filterEvent(event, items) {
+  console.log(event.target.dataset.type);
+  const dataset = event.target.dataset;
+  const type = dataset.type;
+  const value = dataset.value;
+  if (type === null || value === null) {
+    return;
+  }
+  const filtered = items.filter((item) => item[type] === value);
+  console.log(filtered);
+  displayItems(filtered);
+}
 
+function handleEvent(items) {
+  storeBtn.addEventListener("click", displayItems(items));
+  /*oneBtn.addEventListener("click", (items) => {
+    const tShirts = items.filter((item) => item.type === "tshirts");
+    console.log(tShirts);
+  });*/
+  closetList.addEventListener("click", (event) => filterEvent(event, items));
+}
+
+//모든 아이템 보여주기
+function displayItems(items) {
+  ul.innerHTML = "";
+  const li = document.createElement("li");
   const div = items.map(
-    (item) => `<div><img src=${item.image}>${item.gender},${item.size}</div>`
+    (item) =>
+      `<div><img src=${item.image}><span>${item.gender}</span><span>${item.size}</span></div>`
   );
   //console.log(div);
-  //ul.append(li);
+  li.innerHTML = div;
+  ul.append(li);
 }
 
 loadItems()
   .then((items) => {
-    displayItems(items);
+    handleEvent(items);
   })
   .catch(console.log);
 
